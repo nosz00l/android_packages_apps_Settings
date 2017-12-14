d803a0a05fc9802bb18145f74cfd12030d5861e5
ezio84 <brabus84@gmail.com>
2017-12-14 07:57:46 +0100

    Enable notifications led light by default [2/2]

--- src/com/android/settings/notification/ChannelNotificationSettings.java
+++ src/com/android/settings/notification/ChannelNotificationSettings.java
@@ -510,8 +510,8 @@ public class ChannelNotificationSettings extends NotificationSettingsBase {
                 .getBoolean(com.android.internal.R.bool.config_intrusiveNotificationLed)) {
             return false;
         }
-        return /*Settings.System.getInt(getContentResolver(),
-                Settings.System.NOTIFICATION_LIGHT_PULSE, 1) == 1;*/true;
+        return Settings.System.getInt(getContentResolver(),
+                Settings.System.NOTIFICATION_LIGHT_PULSE, 1) == 1;
     }
 
     void updateDependents(boolean banned) {
